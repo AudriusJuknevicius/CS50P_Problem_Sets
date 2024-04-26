f41b95d5dcbb0be9a1afd47e68f245c9953322f8
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-26 12:53:10 +0000

    Fri, Apr 26, 2024, 1:53 PM +01:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,7 +1,7 @@
 # Implementing a program in python that promts the user for input and then outputs that same input, replacing each space with ...(i.e three periods)
 
 def slowdown():
-    userinput = input("State your five favourite places! ")
+    userinput = input("State your five favourite places! ").replace(" ","...")
     print(userinput, sep="...")
 
 slowdown()
