f3641ff4ae569311cafa1457a4588f42eeea7e78
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-26 21:34:09 +0000

    Fri, Apr 26, 2024, 10:34 PM +01:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,4 +1,10 @@
 # E = mc2. Implementing a program that prompts for mass as integer and then outputs equivelent number in Joules as integer.
 
-def einstein ():
-    
+def einstein (j):
+    sol = int(300000000)
+    mass = input int("Please input the mass ")
+    j = sol * mass
+
+
+    einstein ()
+
