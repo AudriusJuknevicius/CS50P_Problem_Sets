8ee7bfa048e5f6d0d0d2dce0bc8b417c618bd4fb
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-26 12:38:21 +0000

    Fri, Apr 26, 2024, 1:38 PM +01:00

---
+++ playback/playback.py
@@ -0,0 +1,5 @@
+# Implementing a program in python that promts the user for input and then outputs that same input, replacing each space with ...(i.e three periods)
+
+def slowdown():
+    userinput = input("Can you tell me the sea shore saying really quickly? ")
+    print(userinput)
