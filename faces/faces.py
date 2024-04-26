f1caedf47af076af95aa8805e9af77380006acac
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-26 12:56:51 +0000

    Fri, Apr 26, 2024, 1:56 PM +01:00

---
+++ faces/faces.py
@@ -0,0 +1,5 @@
+#Implementing a function called "convert" that accepts a str as input and returns the same input with any ":)" converted to 🙂.
+# Also, any ":(" converted to 🙁)
+
+Def convert():
+    
