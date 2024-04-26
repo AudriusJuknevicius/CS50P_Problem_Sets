bb7e9465a2eea4d3054bbfc268d3ce62bda1108b
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-26 12:59:39 +0000

    Fri, Apr 26, 2024, 1:59 PM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,5 +1,6 @@
 #Implementing a function called "convert" that accepts a str as input and returns the same input with any ":)" converted to 🙂.
 # Also, any ":(" converted to 🙁)
 
-Def convert():
-    
+Def main():
+
+    convert = input("Enter a sad/happy emoji! ",).replace(":(","🙂"))
