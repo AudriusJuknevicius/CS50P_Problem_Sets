6be66c085f44adfa1888f27c52f912cb6401a833
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-26 13:01:20 +0000

    Fri, Apr 26, 2024, 2:01 PM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -3,4 +3,7 @@
 
 Def main():
 
-    convert = input("Enter a sad/happy emoji! ",).replace(":(","🙂"))
+    convert = input("Enter a sad/happy emoji! ", ).replace(":)","🙂",":(","🙁")
+    print(convert)
+
+main()
