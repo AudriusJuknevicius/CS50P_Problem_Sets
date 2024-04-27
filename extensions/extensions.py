34bd4774bc285b662c058b787a7a5bf791805b16
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-27 10:00:17 +0000

    Sat, Apr 27, 2024, 11:00 AM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -6,7 +6,20 @@ def main():
     type = input("File name: ").lower().strip()
 
     match type:
-        case type.endswith(".gif")
+        case type.endswith(".gif"):
             print("image/gif")
+        case type.endswith(".jpg") | type.endswith(".jpeg"):
+            print("image/jpeg")
+        case type.endswith(".png"):
+            print("image/png")
+        case type.endswith(".pdf"):
+            print("application/pdf")
+        case type.endswith(".txt"):
+            print("text/plain")
+        case type.endswith(".zip"):
+            print("application/zip")
+        case _:
+            print("application/octet-stream")
 
 
+main()
