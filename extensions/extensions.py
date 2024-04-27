c695de3439e5d6c5ab677123319e6f50092f7723
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-27 09:50:33 +0000

    Sat, Apr 27, 2024, 10:50 AM +01:00

---
+++ extensions/extensions.py
@@ -0,0 +1,11 @@
+# https://cs50.harvard.edu/python/2022/psets/1/extensions/
+
+#Output media type depending on the ending suffix for example cat.gif = image/gif.
+
+def main():
+    type = input("File name: ").lower().strip()
+
+    match type:
+        case 
+
+
