8d03bc6f0ccacf3182d238b287d7ba80b0cf9c5b
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-27 09:29:19 +0000

    Sat, Apr 27, 2024, 10:29 AM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -4,7 +4,7 @@
 #   Outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
 
 def main():
-    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
 
     if answer == "42":
         print("Yes")
