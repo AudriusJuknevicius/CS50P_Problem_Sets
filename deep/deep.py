e174b4f784625bc15657c306b95445dda7d2f8f6
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-27 09:27:10 +0000

    Sat, Apr 27, 2024, 10:27 AM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -6,4 +6,13 @@
 def main():
     answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-    if answer = "42
+    if answer == "42":
+        print("Yes")
+    elif answer == "forty-two":
+        print("Yes")
+    elif answer == "forty two":
+        print("Yes")
+    else: print("No")
+
+
+main()
