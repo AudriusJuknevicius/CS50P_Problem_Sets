d9e68724b781a44423e0e2d56ee1b55dd2fb4b92
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-28 21:37:50 +0000

    Sun, Apr 28, 2024, 10:37 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -10,8 +10,7 @@ def main():
     if len(parts) < 2:
         print("Snake Case:", camelCase)
     elif len(parts) > 1:
-        snake_case = p.join()
-        print("Snake Case:", snake_case,sep="_")
+        print("Snake Case:", parts,sep="_")
 
 
 def finder(s):
