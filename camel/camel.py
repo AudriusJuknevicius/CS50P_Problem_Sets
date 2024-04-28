d367042266f614b139fab28b4a26a0d0d465cb4e
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-28 18:34:40 +0000

    Sun, Apr 28, 2024, 7:34 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,4 +1,5 @@
 # https://cs50.harvard.edu/python/2022/psets/2/camel/
 # Implement a program that promts the user for camel case and the program outputs the corresponding name in snake case.
 
-
+def main():
+    
