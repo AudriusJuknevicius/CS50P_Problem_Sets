2f72df3406be5d3dfc2d49e873aedf4c76aa2f2d
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-28 19:35:14 +0000

    Sun, Apr 28, 2024, 8:35 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,21 +1,15 @@
 # https://cs50.harvard.edu/python/2022/psets/2/camel/
 # Implement a program that promts the user for camel case and the program outputs the corresponding name in snake case.
 
-def main():
-    camelCase = finder(input("camelCase: "))
-
-    def finder(fdr):
-        for fdr.isupper() in fdr:
-            return fdr
-
 
 
 def main():
     camelCase = input("camelCase: ")
 
-    names = finder(camelCase)
-    names = finder(camelCase)
-    print("names",sep="_")
+    if len(parts) <1:
+        print(camelCase)
+    elif len(finder) >1:
+        print("parts",sep="_")
 
 
     def finder(s):
@@ -24,3 +18,5 @@ def main():
                 parts = s.split(char)
                 return parts
             return [s]
+
+main()
