cf63700db6c9c9add215aecd2b59167946a27953
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-28 10:23:44 +0000

    Sun, Apr 28, 2024, 11:23 AM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,10 @@
 # https://cs50.harvard.edu/python/2022/psets/1/meal/
-
-
+# Implement a program that tells the user if its breakfrast, lunch or dinner time when asked.
+# If it's not a meal time, don't output anything at all.
 
 
 def main():
-    ...
+    
 
 
 def convert(time):
