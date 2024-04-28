251473cdefe7f3d0c9a6e5ba5140e2c00e473daf
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-28 09:13:29 +0000

    Sun, Apr 28, 2024, 10:13 AM +01:00

---
+++ interpreter/interpreter.py
@@ -0,0 +1,6 @@
+# https://cs50.harvard.edu/python/2022/psets/1/interpreter/
+
+# Interpreter is a program that prompts the user for an arithmetic expression and then calculates the output.
+
+def main():
+    x
