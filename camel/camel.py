20099ebfec1f58e0c3f6847cb4ba076a6bff1d26
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-28 21:32:42 +0000

    Sun, Apr 28, 2024, 10:32 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -10,7 +10,7 @@ def main():
     if len(parts) < 2:
         print("Snake Case:", camelCase)
     elif len(parts) > 1:
-        snake_case = "_".join(parts)
+        snake_case = join(parts)
         print("Snake Case:", snake_case)
 
 
