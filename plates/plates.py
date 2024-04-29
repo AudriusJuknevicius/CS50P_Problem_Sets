6a58195922f48fffdc8ccf2134a6a114a97f6f1c
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-29 20:34:14 +0000

    Mon, Apr 29, 2024, 9:34 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,4 +1,16 @@
 # https://cs50.harvard.edu/python/2022/psets/2/plates/
-#
-#
-#
+
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    ...
+
+
+main()
