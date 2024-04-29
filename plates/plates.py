466a2c273bc1ea0cadf1029a9472d9d1df53eada
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-29 21:57:22 +0000

    Mon, Apr 29, 2024, 10:57 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -25,12 +25,10 @@ def is_valid_2(s):
         else:
             for numberchar in s:
                 if numberchar.isdigit():
-                    nsplit += 1
-                    if letterchar.isalpha():
-                        return False
-
-                    
-    if lsplit > 1 and  6 <= lsplit + nsplit:
+                 nsplit += 1
+    for s[0:lsplit]:
+        
+    elif lsplit > 1 and  6 <= lsplit + nsplit:
         return True
 
 
