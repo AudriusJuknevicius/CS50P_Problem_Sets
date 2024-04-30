41e9811bac8c4d5445b0a3fdaa470adba93b1dfc
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-30 08:10:36 +0000

    Tue, Apr 30, 2024, 9:10 AM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -3,7 +3,7 @@
 def food():
     rawfruit = input("Item: ").lower().title()
     caloriesoutput = checkfood(rawfruit)
-    if caloriesoutput = caloriesoutput.isdigit():
+    if caloriesoutput.isdigit():
         print("Calories:", caloriesoutput)
 
 
