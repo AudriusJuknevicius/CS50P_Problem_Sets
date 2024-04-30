b1b3f29a9ea5262c1c50927e5891840e712ce8e7
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-30 12:59:16 +0000

    Tue, Apr 30, 2024, 1:59 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,8 +1,7 @@
 # https://cs50.harvard.edu/python/2022/psets/3/fuel/
 
 try:
-    x = 1
-    y = int(input("Fraction: "))
+    x, y = int(input("Fraction: ")).split("/")
 except ValueError:
     print("Fraction is not an integer")
 else:
