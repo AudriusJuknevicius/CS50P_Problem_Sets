fac45af21014f45653b3f78e55374ed185bcf821
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-30 13:09:55 +0000

    Tue, Apr 30, 2024, 2:09 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,12 +1,15 @@
 # https://cs50.harvard.edu/python/2022/psets/3/fuel/
 
-try:
-    x, y = int(input("Fraction: ")).split("/")
-except ValueError:
-    print("Fraction is not an integer")
-else:
-    answer = (100 / y) * x
-    print(answer+"%")
+
+
+def main()
+    try:
+        x, y = int(input("Fraction: ")).split("/")
+    except ValueError:
+             print("Fraction is not an integer")
+    else:
+          answer = (100 / y) * x
+          print(answer+"%")
 
 
 
