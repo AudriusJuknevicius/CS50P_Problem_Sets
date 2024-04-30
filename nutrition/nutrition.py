cb85e85af31c3b7d1cf4d5c80397c824dc586dee
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-04-30 07:37:41 +0000

    Tue, Apr 30, 2024, 8:37 AM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,3 +1,19 @@
 # https://cs50.harvard.edu/python/2022/psets/2/nutrition/
 
+def food():
+    rawfruit = input("Item: ")
+    caloriesoutput = rawfruit(checkfood)
+    print("Calories:", caloriesoutput)
+
+
+
+
+
+def checkfood(rf)
+    fruits = ["Apple", "Avocado", "Banana", "Cantaloupe",
+              "Grapefruit", "Grapes", "Honeydrew Melon", "Kiwifruit",
+              "Lemon", "Lime", "Nectarine", "Orange", "Peach", "Pear",
+              "Pineapple", "Plums", "Strawberries", "Sweet Cherries",
+              "Tangerine", "Watermelon"]
+    calories = ["130", "50", "110", "50", "60", "90", "50", "90", "15", "20",]
 
