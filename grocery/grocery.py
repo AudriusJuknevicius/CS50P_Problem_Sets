653f8c4fd273bf1c44c1a00f67853bebb6f75f19
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-01 01:55:01 +0000

    Wed, May 1, 2024, 2:55 AM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,9 +5,11 @@ def main():
     while True:
              try:
                 item = input().upper()
-                item_dic = {item: 0}
+                # item_dic = {item: 0}
                 if item in item_dic:
-                    item = item [+1]
+                    item_dic[item] += 1
+                else:
+                     item_dic[item] = 1
              except KeyError:
                     pass
              except EOFError:
