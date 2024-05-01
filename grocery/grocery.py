4ae3f519d319ba916dcdf1557035ba6521e5c195
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-01 01:29:18 +0000

    Wed, May 1, 2024, 2:29 AM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,11 +1,15 @@
 def main():
 
-    item_dic = {}
-
 
          while True:
              try:
                  item = input()
+                item_dic = {item: 0}
+             if item in item_dic:
+                   item = [item: +=1]
+            
+
+
 
 
 
