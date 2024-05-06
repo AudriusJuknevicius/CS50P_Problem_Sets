06d62b5bba0d35012497dabc704ed649750ade22
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-06 22:08:38 +0000

    Mon, May 6, 2024, 11:08 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,5 +1,8 @@
 # https://cs50.harvard.edu/python/2022/psets/4/adieu/
 
+
+import inflect
+p = inflect.engine()
 namelist = []
 
 while True:
@@ -7,6 +10,21 @@ while True:
             userinput = input("Name: ")
             namelist.append(userinput)
         except EOFError:
-            print("Adieu, adieu, to" + namelist)
+            if len(namelist) == 1:
+                names = p.join((namelist[0]))
+            print("Adieu, adieu, to", names)
             break
 
+
+
+
+
+
+mylist = p.join(("apple", "banana", "carrot"))
+# "apple, banana, and carrot"
+
+mylist = p.join(("apple", "banana"))
+# "apple and banana"
+
+mylist = p.join(("apple", "banana", "carrot"), final_sep="")
+# "apple, banana and carrot"
