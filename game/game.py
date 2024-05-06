59e349b24fa64f0fbe5dd299feaa20a372ef8dda
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-06 22:50:43 +0000

    Mon, May 6, 2024, 11:50 PM +01:00

--- game/game.py
+++ game/game.py
@@ -10,13 +10,13 @@ while True:
                 answer = random.randint(1, chosenlevel)
                 guess = int(input("Guess: "))
 
-            while True:
-
                 if guess > 0:
                      if guess > answer:
                         print("Too large!")
+
                      elif guess < answer:
                         print("Too small!")
+
                      elif guess == answer:
                         print("Just right!")
                         break
