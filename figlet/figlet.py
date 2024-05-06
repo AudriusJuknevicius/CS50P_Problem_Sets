a08b1d914cc0b5ba96a2d3ac5cca171449e78d61
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-06 16:21:33 +0000

    Mon, May 6, 2024, 5:21 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,6 +5,9 @@ import sys
 import figlet
 
 
+if len(sys.argv) == 0:
+    
+
 # Checks that the provided command-line argument has first -f or --font or --font as the second. Otherwise exits.
 if sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] != "--font":
     sys.exit
