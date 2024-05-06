f8d9daed0954cf6c0d3a1cc4195d3176efb7ec1d
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-06 22:16:00 +0000

    Mon, May 6, 2024, 11:16 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -11,7 +11,7 @@ while True:
             namelist.append(userinput)
         except EOFError:
             if len(namelist) == 1:
-                print("Adieu, adieu, to", namelist[0])
+                print("Adieu, adieu, to", namelist)
 
             elif len(namelist) ==2:
                 names = p.join((namelist[0], namelist[1]))
