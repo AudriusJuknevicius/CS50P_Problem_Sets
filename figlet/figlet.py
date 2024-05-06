25bff928210c69b2d376d52cf810e3c18ffa7566
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-06 16:30:34 +0000

    Mon, May 6, 2024, 5:30 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,17 +5,18 @@ import sys
 import figlet
 
 
+# Checks that the provided command-line argument has first -f or --font or --font as the second. Otherwise exits.
+if sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] != "--font":
+    sys.exit
 
 figlet = figlet(sys.argv)
 
 
 if len(sys.argv) == 0:
+    figlet.setFont(font=f)
     sys.argv = random.choice(figlet.getFonts())
+    print(figlet.renderText(sys.argv))
 
-
-# Checks that the provided command-line argument has first -f or --font or --font as the second. Otherwise exits.
-if sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] != "--font":
-    sys.exit
 
 if len(sys.argv) == 0:
     print(input("Input: ")
