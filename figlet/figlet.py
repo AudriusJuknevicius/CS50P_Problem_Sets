b7bdd1cd1e32ceaad83ec7490e4d223ba731a5e3
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-06 21:44:30 +0000

    Mon, May 6, 2024, 10:44 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,23 +8,30 @@ figlet = Figlet()
 
 #Check if any command-line arguments were provided.
 
-if len(sys.argv) == 1:
+def choice():
+    if len(sys.argv) == 1:
+        return choice1()
+    elif sys.argv[1] == "-f" or sys.argv[1] == "--font":
+        return choice2()
+    else:
+        sys.exit("Too many or few arguments.")
+
+
+
+def choice1():
     user_input = input("Input: ")
     figlet.setFont(font=random.choice(figlet.getFonts()))
     font_changed = figlet.renderText(user_input)
     print("Output: " + font_changed)
-    sys.exit
 
-elif len(sys.argv) == 3:
+def choice2():
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         figlet.setFont(font=sys.argv[2])
         userinput2 = input("Input: ")
         final_font = figlet.renderText(userinput2)
         print("Output: " + final_font)
-    else:
-        sys.exit("Too many or few arguments.")
 
-
+choice()
 
 
 
