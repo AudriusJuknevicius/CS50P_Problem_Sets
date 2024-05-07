b907c52db8338a4083d5444d390e1c2e6f618939
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-07 01:05:04 +0000

    Tue, May 7, 2024, 2:05 AM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -8,11 +8,25 @@ def main():
 
 
 def get_level():
-    ...
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level =! 1 or level =! 2 or level =! 3:
+                return level
+            raise ValueError("Wrong Value")
+
+
 
 
 def generate_integer(level):
-    ...
+    if level == 1:
+
+
+
+    level 1 = 1-9
+    level 2 = 10-99
+    level 3 = 100-999
+
 
 
 if __name__ == "__main__":
