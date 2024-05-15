11e762edacd8aa15f2af0b02f70c0bbb63baf972
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-15 22:26:54 +0000

    Wed, May 15, 2024, 11:26 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,17 +3,19 @@
 import requests
 import sys
 
-try:
-    if len(sys.argv) < 2:
-    sys.exit("Missing command-line argument")
-    elif sys.argv[2] float:
-    sys.exit("Wrong value")
-    else
-break
+# try:
+#     if len(sys.argv) < 2:
+#     sys.exit("Missing command-line argument")
+#     elif sys.argv[2] float:
+#     sys.exit("Wrong value")
+#     else
+# break
 
 bitcoin_json = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
 bitcoin_api = bitcoin_json.json()
+
+for bitcoin in bitcoin_api
 
 
 except requests.RequestException:
