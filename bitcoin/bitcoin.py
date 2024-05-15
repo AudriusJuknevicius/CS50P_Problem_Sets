a1a11921607ed83802559f2a1003c3418c3a34e5
AudriusJuknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-05-15 23:01:23 +0000

    Thu, May 16, 2024, 12:01 AM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,12 +6,12 @@ import sys
 try:
     if len(sys.argv) < 2:
         sys.exit("Missing command-line argument")
-    except ValueError:
+    valuetimes = float(sys.argv[1])
+except ValueError:
         sys.exit("Command-line argument is not a number")
 except requests.RequestException:
         sys.exit
 
-valuetimes = float(sys.argv[1])
 
 bitcoin_json = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 bitcoin_api = bitcoin_json.json()
