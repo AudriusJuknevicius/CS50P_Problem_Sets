354c2867abf26479cb1d77560dbcc236c3fd934b
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-19 19:49:07 +0000

    Thu, Sep 19, 2024, 8:49 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,39 +1,12 @@
+# https://cs50.harvard.edu/python/2022/psets/5/test_twttr/
+
 from twttr import shorten
 import pytest
 
-
 def test_twittr():
-
-    try:
-        assert shorten("Education") == "dctn"
-    except AssertionError:
-        print("Failed to remove vowels correctly")
-
-    try:
-        assert shorten("HELLO") == "HLL"
-    except AssertionError:
-        print("Failed to remove uppercase vowels")
-
-    try:
-        assert shorten("hello") == "hll"
-    except AssertionError:
-        print("Failed to remove lowercase vowels")
-
-    try:
-        assert shorten("H3ll0 W0rld!") == "H3ll0 W0rld!"
-    except AssertionError:
-        print("Failed with numbers and punctuation")
-
-    try:
-        assert shorten("rhythm") == "rhythm"
-    except AssertionError:
-        print("Failed with string containing no vowels")
-
-    try:
-        assert shorten("") == ""
-    except AssertionError:
-        print("Failed with an empty string")
-
-
-
-
+    assert shorten("Education") == "dctn"
+    assert shorten("HELLO") == "HLL"
+    assert shorten("hello") == "hll"
+    assert shorten("H3ll0 W0rld!") == "H3ll0 W0rld!"
+    assert shorten("rhythm") == "rhythm"
+    assert shorten("") == ""
