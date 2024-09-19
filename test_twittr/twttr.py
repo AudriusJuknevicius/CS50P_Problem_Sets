f8c03dcc5b0183ccc4c6f519053f11b521cb8a7c
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-19 18:46:59 +0000

    Thu, Sep 19, 2024, 7:46 PM +01:00

--- test_twittr/twttr.py
+++ test_twittr/twttr.py
@@ -1,11 +1,3 @@
-# https://cs50.harvard.edu/python/2022/psets/2/twttr/
-# When texting or tweeting, it’s not uncommon to shorten words to save time or space, as by omitting
-# vowels,much like Twitter was originally called twttr.
-
-# In a file called twttr.py, implement a program that prompts the user for a str of text and
-# then outputs that same text but with all vowels (A, E, I, O, and U) omitted,
-# whether inputted in uppercase or lowercase.
-
 def main():
     tweet = input("Input: ")
     tweet = change(tweet)
@@ -23,3 +15,15 @@ def change(t):
 
 
 main()
+
+
+def main():
+    
+
+
+def shorten(word):
+
+
+
+if __name__ == "__main__":
+    main()
