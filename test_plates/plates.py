1772a30e85388f64ed9fb7b648810489d63becb1
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-21 20:14:09 +0000

    Sat, Sep 21, 2024, 9:14 PM +01:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -16,9 +16,9 @@ def is_valid_1(s):
     if s[:2].isalpha():
         if len(s) < 2 or len(s) > 6:
             return False
-        else
+        else:
             return True
-    else
+    else:
         return False
 
 
