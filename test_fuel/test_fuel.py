09a14fab58acb458aaf103ed3d22ef09bb5eaef2
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-21 21:46:41 +0000

    Sat, Sep 21, 2024, 10:46 PM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,9 +2,9 @@ import pytest
 from fuel import gauge, convert
 
 def gauge():
-    assert gauge("1") == "E"
+    assert gauge("0/1") == "E"
     assert gauge("99") == "F"
-    assert gauge("50") == "50%"
+    assert gauge("5/10") == "50%"
 
 def convert():
     assert convert("CS50") == ValueError
