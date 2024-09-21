c618e53aa74c5ef10d43c039b4bb22855e911518
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-21 21:03:31 +0000

    Sat, Sep 21, 2024, 10:03 PM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,14 +2,14 @@ import pytest
 from fuel import gauge, convert
 
 def gauge():
-    assert gauge("CS50") == True
-    assert gauge("CS05") == False
-    assert gauge("CS50P") == False
+    assert gauge("1") == "E"
+    assert gauge("99") == "F"
+    assert gauge("50") == "50%"
 
 def convert():
-    assert convert("CS50") == True
-    assert convert("CS05") == False
-    assert convert("CS50P") == False
+    assert convert("CS50") ==
+    assert convert("CS05") ==
+    assert convert("CS50P") == 
 
 
 
