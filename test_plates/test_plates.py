29c2cf4f0544ec1b1e7d10649bc133b50fdcd549
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-21 19:54:37 +0000

    Sat, Sep 21, 2024, 8:54 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,10 +2,10 @@ import pytest
 from plates import is_valid
 
 def test_plates():
-    assert is_valid("hello") == 0
-    assert is_valid("HELLO") == 0
-    assert is_valid("hey") == 20
-    assert is_valid("H3ll0 W0rld!") == 20
-    assert is_valid("Yo") == 100
-    assert is_valid("") == 100
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("CS50P") == False
+    assert is_valid("P13.14") == False
+    assert is_valid("H") == False
+    assert is_valid("") == False
 
