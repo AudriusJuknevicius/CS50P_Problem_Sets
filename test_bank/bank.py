b16229fb4e169f4ed64cfe6b9ed46066ed5cfdd5
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-21 18:03:36 +0000

    Sat, Sep 21, 2024, 7:03 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,10 +1,3 @@
-# https://cs50.harvard.edu/python/2022/psets/1/bank/
-# In a file called bank.py, implement a program that prompts the user for a greeting.
-# If the greeting starts with “hello”, output $0.
-# If the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100.
-# Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
-
-
 def main():
     greeting = input("Greeting: ").lower().strip()
 
