c5139288fc2dcfb1fb925c7fedaece3e8626cc73
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-21 18:56:04 +0000

    Sat, Sep 21, 2024, 7:56 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -16,3 +16,15 @@ def value(greeting):
 
 if __name__ == "__main__":
     main()
+
+def main():
+    greeting = input("Greeting: ").lower().strip()
+
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
