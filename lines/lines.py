1f9ddd731e05b3e26a65f1db8e17ef6b775e1e6c
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-25 14:20:46 +0000

    Wed, Sep 25, 2024, 3:20 PM +01:00

---
+++ lines/lines.py
@@ -0,0 +1,40 @@
+import sys
+
+def main():
+    if len(sys.argv) != 2: # Checks if it has exactly one in-line argument.
+        sys.exit("Too many or few in-line arguments.")
+
+    filename = sys.argv[1] # Identify the file name from the argument.
+
+    if not filename.endswith(".py"): # Checks if the file ends with .py
+        sys.exit("Not a Python file")
+
+        print(f"Processing file: {filename}") # Continues with the program if the previous two conditions are correct.
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+#     lambda name, file = sys.argv[1].split(".")
+#     if len(sys.argv[1],if file == "py" return True else: sys.exit) >= 1:
+#         return sys.argv[1]
+#     if file == "py":
+#         return True
+#     else:
+#         sys.exit
+
+
+#     if len(sys.argv[1],if file == "py" return True else: sys.exit) >= 1:
+#         return sys.argv[1]
+#         else:
+#             sys.exit
+
+
+
+# main()
