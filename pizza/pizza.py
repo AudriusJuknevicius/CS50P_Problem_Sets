e61a81e4c8901485c53ed5301232ea569c3ec14b
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-25 16:42:37 +0000

    Wed, Sep 25, 2024, 5:42 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,4 +1,5 @@
 import sys
+from tabulate import tabulate
 
 if len(sys.argv) != 2: # Checks if it has exactly one in-line argument.
         sys.exit("Too many or few in-line arguments.")
@@ -7,4 +8,7 @@ filename = sys.argv[1] # Identify the file name from the argument.
 
 if not filename.endswith(".csv"): # Checks if the file ends with ".py".
         sys.exit("Not a Comma Seperated Value file")
+
+with open(filename) as pizzycsv:
+    pretty = csv.reader(pizzycsv)
 
