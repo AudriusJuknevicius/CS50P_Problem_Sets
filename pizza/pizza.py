288ddd61181b9bb82300c8bb782224f818945154
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-25 16:56:46 +0000

    Wed, Sep 25, 2024, 5:56 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -11,6 +11,6 @@ if not filename.endswith(".csv"): # Checks if the file ends with ".py".
         sys.exit("Not a Comma Seperated Value file")
 
 with open(filename) as pizzycsv:
-    reader = csv.reader(pizzycsv)
-    header = next(reader)
-    print(tabulate(reader, header, tablefmt="grid"))
+    reader = csv.reader(pizzycsv) # Reader variable reads the CSV file.
+    header = next(reader) # Extracts the first line from the CSV file.
+    print(tabulate(reader, header, tablefmt="grid")) # Prints whilst seperating the first line with the contents, in the grid format.
