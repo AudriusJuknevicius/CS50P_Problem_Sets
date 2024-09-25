fedda37b5afe1632bdd0e4012c11114c31f69663
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-25 22:51:00 +0000

    Wed, Sep 25, 2024, 11:51 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -15,7 +15,7 @@ students = []
 with open(filename1, "r") as before:
         reader = csv.DictReader(before)
         for row in reader:
-                last, first = 
+                last, first = row["name"].split(",")
                 students.append({"name": row["name"], "house": row["house"]})
 
 
