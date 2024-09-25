1ae2a4f9efac9b1623d99a67f2e908a5ad9c5816
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-25 22:16:20 +0000

    Wed, Sep 25, 2024, 11:16 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,8 +4,9 @@ import csv
 if len(sys.argv) != 3: # Checks if it has exactly one in-line argument.
         sys.exit("Too many or few in-line arguments.")
 
-filename = sys.argv[1] # Identify the file name from the argument.
+filename1 = sys.argv[1] # Identify the first file name from the argument.
+filename2 = sys.argv[2] # Identify the second file name from the argument.
 
-if not filename.endswith(".csv"): # Checks if the file ends with ".py".
+if not filename1.endswith(".csv"): # Checks if the file ends with ".csv".
         sys.exit("Not a Comma Seperated Value file")
 
