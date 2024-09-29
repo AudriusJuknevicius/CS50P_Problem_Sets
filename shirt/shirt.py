b93bcc8d175c17d2727d6c36dfdc2fff211cacb2
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-29 11:31:14 +0000

    Sun, Sep 29, 2024, 12:31 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -10,43 +10,23 @@ filename2 = sys.argv[2] # Identify the second file name as the output.
 if not filename1.lower().endswith((".jpg", ".jpeg", ".png")):  # Checks if the first input has the right file type.
         sys.exit("File must end with .jpeg, .jpeg or .png")
 
-fe1 = filename1.lower().split(".") # Splits filename 1 and 2 to just their extensions.
-fe2 = filename2.lower().split(".")
+filename1front, fe1 = filename1.lower().split(".") # Splits filename 1 and 2 to just their extensions.
+filename2front, fe2 = filename2.lower().split(".")
 
 if not fe1 == fe2:
-        sys.exit("Input extension is not the same as the output's")
+        sys.exit("Input extension is not the same as the output's") # Checks input and output extensions.
+
+try:
+        person = Image.open(filename1) # Opens before file image.
+        shirt = Image.open("shirt.png") # Opens the shirt image file.
+
+        shirtsize = shirt.size # shirtsize variable gains the size information from shirt image file.
+
+        person_resized = ImageOps.fit(person, shirtsize) # 
+        person_resized.paste(shirt, shirt)
+        person_resized.save(filename2)
+except FileNotFoundError:
+    sys.exit("Could not read" + filename1)
 
 
-person = Image.open(filename1)
-shirt = Image.open("shirt.png")
-
-shirtsize = shirt.size
-
-person_resized = ImageOps.fit(person, shirtsize)
-person_resized.paste(shirt, shirt)
-person_resized.save(filename2)
-
-
-
-
-
-# students = []
-
-# try:
-#     with open(filename1, "r") as before:
-#             reader = csv.DictReader(before)
-#             for row in reader:
-#                     last, first = row["name"].split(", ")
-#                     students.append({"first": first, "last": last, "house": row["house"]})
-# except FileNotFoundError:
-#     sys.exit("Could not read" + filename1)
-
-
-
-
-# with open(filename2, "w") as after:
-#         writer = csv.DictWriter(after, fieldnames=["first", "last", "house"])
-#         writer.writeheader()
-#         for student in students:
-#                writer.writerow(student)
 
