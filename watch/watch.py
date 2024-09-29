f641b60d05601d612fe62dcde1d0781ee7c8e994
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-29 21:02:30 +0000

    Sun, Sep 29, 2024, 10:02 PM +01:00

--- watch/watch.py
+++ watch/watch.py
@@ -8,7 +8,7 @@ def main():
 
 def parse(s):
     if matches := re.search(r"(https?)://(?:www\.)?youtube\.com/embed/(\w+)", s, re.IGNORECASE):
-        return matches.group(1) + "://youtu.be/" + matches.group(2)
+        return "https://youtu.be/" + matches.group(2)
 
 
 
