bd8974696607cc1ac57d0bddde345a6959486b63
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-29 10:23:06 +0000

    Sun, Sep 29, 2024, 11:23 AM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,8 +4,8 @@ from PIL import image
 if len(sys.argv) != 3: # Checks if it has exactly one in-line argument.
         sys.exit("Too many or few in-line arguments.")
 
-filename1 = sys.argv[1] # Identify the first file name from the argument.
-filename2 = sys.argv[2] # Identify the second file name from the argument.
+filename1 = sys.argv[1] # Identify the first file name as the input.
+filename2 = sys.argv[2] # Identify the second file name as the output.
 
 if not filename1.endswith(".py"): # Checks if the file ends with ".csv".
         sys.exit("Not a Comma Seperated Value file")
