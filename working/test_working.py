76359ff0e9eca105efb8e9f13d206ab1d47c4a45
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:54:17 +0000

    Mon, Sep 30, 2024, 2:54 PM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -6,8 +6,9 @@ def test_convert():
     assert test_convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert test_convert("9:00 AM to 5 PM") == "09:00 to 17:00"
     assert test_convert("9 AM to 5:00 PM") == "09:00 to 17:00"
-    assert test_convert("") == "09:00 to 17:00"
-    assert test_convert("") == "09:00 to 17:00"
-    assert test_convert("") == "09:00 to 17:00"
-    assert test_convert("") == "09:00 to 17:00"
-
+    assert test_convert("12:00 AM to 1:00 AM") == "00:00 to 01:00"
+    assert test_convert("12 AM to 1 AM") == "00:00 to 01:00"
+    assert test_convert("12:00 PM to 1:00 PM") == "12:00 to 13:00"
+    assert test_convert("12 PM to 1 PM") == "12:00 to 13:00"
+    with pytest.raises(ValueError): test_convert("9:60 AM to 5:00 PM")
+    with pytest.raises(ValueError): test_convert("9:00 AM to 5:60 PM")
