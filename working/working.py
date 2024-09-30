3af19445721248721823bb79521d2eb04f045932
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:35:49 +0000

    Mon, Sep 30, 2024, 2:35 PM +01:00

--- working/working.py
+++ working/working.py
@@ -8,17 +8,19 @@ def main():
 
 def convert(s):
     if matches := re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$", s, re.IGNORECASE):
-            hours1 = matches.group(1)
-            minutes1 = matches.group(2)
-            meridiem1 = matches.group(3)
-            hours2 = matches.group(4)
-            minutes2 = matches.group(5)
-            meridiem2 = matches.group(6)
-        return matches.groups()
+        hours1 = matches.group(1)
+        minutes1 = matches.group(2)
+        meridiem1 = matches.group(3)
+        hours2 = matches.group(4)
+        minutes2 = matches.group(5)
+        meridiem2 = matches.group(6)
 
 
 
 def check_minutes():
+    if 
+
+
 
 
 
