423f42c2a0b1d8bea65d69f2a0c3ae3707f36d91
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:41:45 +0000

    Mon, Sep 30, 2024, 2:41 PM +01:00

--- working/working.py
+++ working/working.py
@@ -11,17 +11,19 @@ def convert(s):
         hours1 = matches.group(1)
         minutes1 = matches.group(2)
         meridiem1 = matches.group(3)
-
         hours2 = matches.group(4)
         minutes2 = matches.group(5)
         meridiem2 = matches.group(6)
 
 
 
-def check_minutes():
-    if
-
-
+def convert_to_24(hours, meridiem):
+    hours = int(hours)
+    if meridiem == "AM" and hours == 12:
+        return 0  # Midnight case
+    elif meridiem == "PM" and hours != 12:
+        return hours + 12  # Convert to 24-hour time for PM, but not for noon
+    return hours
 
 
 if __name__ == "__main__":
