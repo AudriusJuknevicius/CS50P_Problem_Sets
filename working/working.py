1db274da408d32168a1506057e5fa3c6d24cdeba
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:38:16 +0000

    Mon, Sep 30, 2024, 2:38 PM +01:00

--- working/working.py
+++ working/working.py
@@ -11,6 +11,7 @@ def convert(s):
         hours1 = matches.group(1)
         minutes1 = matches.group(2)
         meridiem1 = matches.group(3)
+        
         hours2 = matches.group(4)
         minutes2 = matches.group(5)
         meridiem2 = matches.group(6)
@@ -18,7 +19,7 @@ def convert(s):
 
 
 def check_minutes():
-    if 
+    if
 
 
 
