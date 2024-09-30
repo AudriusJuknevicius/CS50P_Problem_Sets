256486df00b4536a5a8f5c182fae9be89671b758
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:27:27 +0000

    Mon, Sep 30, 2024, 2:27 PM +01:00

--- working/working.py
+++ working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     if matches := re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$", s, re.IGNORECASE):
-        return matches
+        return matches.groups()
 
 # def time_24hrs(time)
 #     hours, minutes = map(float, time.split(":"))
