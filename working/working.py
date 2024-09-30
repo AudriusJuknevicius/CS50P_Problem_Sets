864b12373ac8ef1ac9191263b1129ef4f900001e
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:03:19 +0000

    Mon, Sep 30, 2024, 2:03 PM +01:00

--- working/working.py
+++ working/working.py
@@ -7,10 +7,9 @@ def main():
 
 
 def convert(s):
-    ...
-
-
-...
+    if matches := re.search(r"(https?)://(?:www\.)?youtube\.com/embed/(\w+)", s, re.IGNORECASE):
+if matches:
+    print(f"Username:", matches.group(2))
 
 
 if __name__ == "__main__":
