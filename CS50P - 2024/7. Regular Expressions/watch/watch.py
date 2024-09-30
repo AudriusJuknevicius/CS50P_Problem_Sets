f1fe91c96079c2970a04c21862db77190b068ded
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-09-30 13:23:47 +0000

    Mon, Sep 30, 2024, 2:23 PM +01:00

--- CS50P - 2024/7. Regular Expressions/watch/watch.py
+++ CS50P - 2024/7. Regular Expressions/watch/watch.py
@@ -10,10 +10,10 @@ def parse(s):
     if matches := re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$")", s, re.IGNORECASE):
         return matches
 
-def time_24hrs(time)
-    hours, minutes = map(float, time.split(":"))
-    time_24h = hours + (minutes / 60)
-    return time_24h
+# def time_24hrs(time)
+#     hours, minutes = map(float, time.split(":"))
+#     time_24h = hours + (minutes / 60)
+#     return time_24h
 
 
 
--- working/working.py
+++ working/working.py
@@ -7,10 +7,16 @@ def main():
 
 
 def convert(s):
-    if matches := re.search(r"(https?)://(?:www\.)?youtube\.com/embed/(\w+)", s, re.IGNORECASE):
-if matches:
-    print(f"Username:", matches.group(2))
+    if matches := re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$"), s, re.IGNORECASE):
+        return matches
+
+# def time_24hrs(time)
+#     hours, minutes = map(float, time.split(":"))
+#     time_24h = hours + (minutes / 60)
+#     return time_24h
+
 
 
 if __name__ == "__main__":
     main()
+
