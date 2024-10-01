a4925259bd2b800caf21c0611b37ca8c118b313b
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-01 09:41:04 +0000

    Tue, Oct 1, 2024, 10:41 AM +01:00

--- response/response.py
+++ response/response.py
@@ -1,6 +1,13 @@
 from validator_collection import validators
 
 
+
+def main():
+    print(email_validate(input("Email: ")))
+
+
 def email_validate(e):
     e = validators.email
 
+if __name__ == "__main__":
+    main()
