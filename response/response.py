f33d11b52b7c59000ebe41959ea09166fbb1a03c
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-01 09:50:09 +0000

    Tue, Oct 1, 2024, 10:50 AM +01:00

--- response/response.py
+++ response/response.py
@@ -12,5 +12,32 @@ def email_validate(e):
     else:
         return "Invalid"
 
+
+email_address = validators.email('this-is-an-invalid-email')
+# Will raise a ValueError
+
+try:
+    email_address = validators.email(None)
+    # Will raise an EmptyValueError
+except errors.EmptyValueError:
+    # Handling logic goes here
+except errors.InvalidEmailError:
+    # More handlign logic goes here
+
+e = validators.email(None, allow_empty = True)
+# The value of email_address will now be None
+
+e = validators.email('', allow_empty = True)
+# The value of email_address will now be None
+
+is_email_address = checkers.is_email('test@domain.dev')
+# The value of is_email_address will now be True
+
+is_email_address = checkers.is_email('this-is-an-invalid-email')
+# The value of is_email_address will now be False
+
+is_email_address = checkers.is_email(None)
+# The value of is_email_address will now be False
+
 if __name__ == "__main__":
     main()
