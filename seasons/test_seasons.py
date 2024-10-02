1fdf096e35fe05ac6558e43d95a53119a0fee16d
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 19:03:35 +0000

    Wed, Oct 2, 2024, 8:03 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,18 +1,8 @@
 import pytest
 from seasons import date2time
-from datetime import date, timedelta
+
+    today = date.tiday()
+    
 
 def test_date2time():
-    today = date.today()
-
-    # Define dates like yesterday and the day before yesterday
-    yesterday = today - timedelta(days=1)
-    day_before_yesterday = today - timedelta(days=2)
-
-    # Calculate expected minute values for the differences
-    one_day_in_minutes = 1 * 1440  # 1 day in minutes
-    two_days_in_minutes = 2 * 1440  # 2 days in minutes
-
-    # Test cases for known differences
-    assert str(one_day_in_minutes) in date2time(str(yesterday))  # 1 day in minutes
-    assert str(two_days_in_minutes) in date2time(str(day_before_yesterday))  # 2 days in minutes
+    assert date2time("hello, um, world") == 1
