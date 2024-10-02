18f21d4f271d7e6d8b9d6a20e8d10d03ff4a2fd3
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 18:48:36 +0000

    Wed, Oct 2, 2024, 7:48 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta
 
 def test_date2time():
     today = date.today()
-    assert date2time("") == 1
-    assert count("Um, thanks for the album.") == 1
-    assert count("yummy") == 0
-    assert count("um, thanks, um....") == 2
+    assert date2time(today.day - 365) == 365
+    # assert count("Um, thanks for the album.") == 1
+    # assert count("yummy") == 0
+    # assert count("um, thanks, um....") == 2
