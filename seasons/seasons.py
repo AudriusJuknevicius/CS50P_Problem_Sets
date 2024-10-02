54e369605b289628ac6a895243d0c446422c3b9c
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 18:40:53 +0000

    Wed, Oct 2, 2024, 7:40 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,14 +8,18 @@ def main():
 
 
 def date2time(userinput):
-    if not birth = date.fromisoformat(userinput):
-        sys.exit()
-        today = date.today()
-        totaltime = today - birth
-        if not totaltime.days <=0:
-            return str.capitalize((p.number_to_words(totaltime.days * 1440, andword="") + " minutes"))
-        else:
-            sys.exit()
+    try:
+        birth = date.fromisoformat(userinput)
+    except ValueError:
+        sys.exit("Invalid date format. Please use YYYY-MM-DD.")
+
+    today = date.today()
+    totaltime = today - birth
+
+    if totaltime.days > 0:
+        return str.capitalize((p.number_to_words(totaltime.days * 1440, andword="") + " minutes"))
+    else:
+        sys.exit("Invalid Date. The date must be in the past")
 
 
 
