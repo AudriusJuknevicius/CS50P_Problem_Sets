7858a06260e0f08cb386700a3cd80cf1202e6553
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 19:26:16 +0000

    Wed, Oct 2, 2024, 8:26 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,11 +2,11 @@ import pytest
 from seasons import date2time
 from datetime import date, timedelta
 
-
-
 def test_date2time():
     today = date.today()
-    year = today - 365
+    one_year_ago = today - timedelta(days=365)
 
-    assert date2time(year) == "Five hundred twenty-five thousand, six hundred minutes"
+    expected_minutes = (today - one_year_ago).days * 24 * 60
+    expected_output = f"{expected_minutes} minutes"
 
+    assert date2time(str(one_year_ago)) == "Five hundred twenty-five thousand, six hundred minutes"
