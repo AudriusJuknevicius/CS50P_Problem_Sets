2d4b5000b0a78ca21bf05ba590b4a39f3b9850c5
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 18:46:44 +0000

    Wed, Oct 2, 2024, 7:46 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,8 +1,10 @@
 import pytest
 from seasons import date2time
+from datetime import date, timedelta
 
 def test_date2time():
-    assert date2time("hello, um, world") == 1
+    today = date.today()
+    assert date2time("") == 1
     assert count("Um, thanks for the album.") == 1
     assert count("yummy") == 0
     assert count("um, thanks, um....") == 2
