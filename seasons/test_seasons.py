6b53808e382161f2f547b06ea32a8cfbb9f8f6b2
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 19:21:00 +0000

    Wed, Oct 2, 2024, 8:21 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,13 +1,13 @@
-# import pytest
-# from seasons import date2time
+import pytest
+from seasons import date2time
 
 
-# def test_date2time():
-# today = date.today()
-# answer = str.capitalize((p.number_to_words(totaltime.days * 1440, andword="") + " minutes"))
+def test_date2time():
+today = date.today()
+answer = str.capitalize((p.number_to_words(totaltime.days * 1440, andword="") + " minutes"))
 
 
-# assert date2time("2000-12-12") == str.capitalize((p.number_to_words(.days * 1440, andword="") + " minutes"))
-# assert date2time("2007-12-31") == answer
+assert date2time("2000-12-12") == str.capitalize((p.number_to_words(.days * 1440, andword="") + " minutes"))
+assert date2time("2007-12-31") == answer
 
 
