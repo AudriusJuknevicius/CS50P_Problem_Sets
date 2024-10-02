11253ad7f0b9a829e8a27c6521c842a98422313f
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-10-02 19:10:59 +0000

    Wed, Oct 2, 2024, 8:10 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -5,5 +5,5 @@ from seasons import date2time
 def test_date2time():
 today = date.today()
 str.capitalize((p.number_to_words(totaltime.days * 1440, andword="") + " minutes"))
-    assert date2time("2000-12-12") == "Eleven million, nine hundred forty-four thousand, eight hundred minutes"
+    assert date2time("2000-12-12") == Date2time
     assert date2time("2007-12-31") == "Eight million, two hundred thirty-six thousand, eight hundred minutes"
