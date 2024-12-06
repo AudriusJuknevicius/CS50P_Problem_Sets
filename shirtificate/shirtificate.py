7546403b884056d7022cce4f38c806967355dea8
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 23:07:51 +0000

    Fri, Dec 6, 2024, 11:07 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,24 +1,14 @@
 from fpdf import FPDF
 
-
-class PDF(FPDF):
-    def header(self):
-        # Setting font: helvetica bold 24
-        self.set_font("helvetica", style="B", size=24)
-        # Setting colors for text:
-        self.set_text_color(255,255,255)
-        # Setting thickness of the frame (1 mm)
-        self.set_line_width(3)
+def main():
+    pdf = PDF()
+    pdf.add_page()
+    pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png")
+    pdf.set_title("CS50 Shirtificate")
+    pdf.set_author("Audrius Juknevicius")
+    pdf.output("shirtificate.pdf")
 
 
-
-
-pdf = PDF()
-pdf.add_page()
-pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png")
-pdf.set_title("CS50 Shirtificate")
-pdf.set_author("Audrius Juknevicius")
-pdf.output("shirtificate.pdf")
 
 if __name__ == "__main__":
     main()
