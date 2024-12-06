15064845451a4a20ea35f9142d350883c267b163
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 13:17:18 +0000

    Fri, Dec 6, 2024, 1:17 PM +00:00

---
+++ jar/jar.py
@@ -0,0 +1,34 @@
+#https://cs50.harvard.edu/python/2022/psets/8/jar/
+
+from jar import Jar
+
+
+class Jar:
+    def __init__(self, capacity=12):
+        self._capacity = capacity
+        self._size = 0
+        capacity return _capacity
+        size return _size
+
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        if n + self._capacity >= 12
+            raise ValueError
+        else:
+            self._size = n + self._size
+
+
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
