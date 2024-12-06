8ac64bba21a30cc5c91a2b9facbc771f629c113c
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 22:42:21 +0000

    Fri, Dec 6, 2024, 10:42 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF
 
+
 pdf = FPDF()
 pdf.add_page(orientation="P", format="A4")
 pdf.set_font('helvetica', size=12)
@@ -8,3 +9,6 @@ pdf.output("hello_world.pdf")
 
 if __name__ == "__main__":
     main()
+
+pdf = PDF()
+
