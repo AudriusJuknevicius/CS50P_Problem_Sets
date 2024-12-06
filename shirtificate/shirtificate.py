f1b04d895df0eda1424e03a81d2ccb2498700154
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 23:24:01 +0000

    Fri, Dec 6, 2024, 11:24 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,7 +5,6 @@ def main():
     pdf.add_page()
     pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", x="C",y=+70)
     pdf.set_font("helvetica", style="B", size=24)
-    
     pdf.set_title("CS50 Shirtificate")
     pdf.set_author("Audrius Juknevicius")
     pdf.output("shirtificate.pdf")
