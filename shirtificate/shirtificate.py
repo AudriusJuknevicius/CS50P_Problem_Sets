5a2018197e49f2f11c21018ba150822a4dae4e2f
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 23:42:37 +0000

    Fri, Dec 6, 2024, 11:42 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -11,10 +11,12 @@ def main():
     pdf.set_text_color(0, 0, 0)
     pdf.cell(0, 50, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align="C")
 
-    pdf.set_font("helvetica", style="B", size=36)
-    pdf.set_text_colour(255, 255, 255)
-    pdf.text(x=55, y=140, txt="Audrius Juknevicius took CS50")
     pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", x="C",y=+70)
+
+    pdf.set_font("helvetica", style="B", size=24)
+    pdf.set_text_color(255, 255, 255)
+    pdf.text(x=42, y=140, text="Audrius Juknevicius took CS50")
+
     pdf.output("shirtificate.pdf")
 
 
