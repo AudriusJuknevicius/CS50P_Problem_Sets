4c12f809d7ce83babfb88ee21f0be9eb67fd3c95
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 22:38:32 +0000

    Fri, Dec 6, 2024, 10:38 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,10 +1,8 @@
 from fpdf import FPDF
 
 pdf = FPDF()
-pdf.add_page()
+pdf.add_page(orientation="P", format="A4")
 pdf.set_font('helvetica', size=12)
 pdf.cell(text="hello world")
 pdf.output("hello_world.pdf")
 
-def add_page(self, orientation="P", format="A4")
-    self._add_page = orientation, format
