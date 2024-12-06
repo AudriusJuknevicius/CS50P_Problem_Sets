0953c198ba71e304287a87cb5e5ea546e5ddfc70
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 23:32:17 +0000

    Fri, Dec 6, 2024, 11:32 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,11 +3,11 @@ from fpdf import FPDF
 def main():
     pdf = FPDF()
     pdf.add_page()
-    pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", x="C",y=+70)
-    pdf.set_font("helvetica", style="B", size=24)
-    pdf.set_text_color(0, 0, 0)
     pdf.set_title("CS50 Shirtificate")
     pdf.set_author("Audrius Juknevicius")
+    pdf.set_font("helvetica", style="B", size=24)
+    pdf.set_text_color(0, 0, 0)
+    pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", x="C",y=+70)
     pdf.output("shirtificate.pdf")
 
 
