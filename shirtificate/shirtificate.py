ee4d6dd517aab2f2f805373dfc98c8f3d3713886
Audrius Juknevicius <137508657+AudriusJuknevicius@users.noreply.github.com>
2024-12-06 22:53:12 +0000

    Fri, Dec 6, 2024, 10:53 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,21 +1,6 @@
 from fpdf import FPDF
 
 
-pdf = FPDF()
-pdf.add_page(orientation="P", format="A4")
-pdf.set_font('helvetica', size=12)
-pdf.cell(text="hello world")
-pdf.output("hello_world.pdf")
-
-if __name__ == "__main__":
-    main()
-
-pdf = PDF()
-
-
-from fpdf import FPDF
-
-
 class PDF(FPDF):
     def header(self):
         # Setting font: helvetica bold 15
